1116058703ed2b83a9ed63d3dc7ed66389a942cb
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-06-04 15:49:25 +0100

    attempt no1 to add some fetch and button click and submit functions but not currently working

--- public/react/components/App.jsx
+++ public/react/components/App.jsx
@@ -1,30 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { Header } from './Header'
 import { Footer } from './Footer';
+import { ItemsList } from './ItemsList';
+
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
-import { ItemsList } from './ItemsList';
 
 
 export const App = () => {
 
 	const [items, setItems] = useState([]);
-  const [formIsActive, setFormIsActive] = useState(false)
+	const [selectedItem, setSelectedItem] = useState(null);
+	const [isAddingItem, setIsAddingItem] = useState(false);
+    const [formIsActive, setFormIsActive] = useState(false)
 
-	async function fetchItems(){
-		try {
-			const response = await fetch(`${apiURL}/items`);
-			const itemsData = await response.json();
-			
-			setItems(itemsData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
+	function goHome() {
+		setSelectedItem(null)
+		setIsAddingItem(false)
+	}
+
+	async function handleClick(slug) {
+		const response = await fetch(`${apiURL}/items/${slug}`)
+		const pageData = await response.json()
+		setSelectedItem(pageData)
+	  }
+
+	async function addItem(item) {
+		const response = await fetch(`${apiURL}/items`, {
+		  method: 'POST',
+		  headers: {
+			'Content-Type': 'application/json'
+		  },
+		  body: JSON.stringify(item)
+		})
+    
+		const newItem = await response.json()
+		setItems([...items, newItem])
+		setIsAddingItem(false)
 	}
 
 	useEffect(() => {
+		async function fetchItems(){
+			try {
+				const response = await fetch(`${apiURL}/items`);
+				const itemsData = await response.json();
+				
+				setItems(itemsData);
+			} catch (err) {
+				console.log("Oh no an error! ", err)
+			}
+		}
+
 		fetchItems();
 	}, []);
+
+	useEffect(() => {
+	  if (selectedItem) {
+		document.title = `${selectedItem.title} - Inventory App`
+	  } else if (isAddingItem) {
+		document.title = 'Add an item - Inventory App'
+	  } else {
+		document.title = 'Inventory App'
+	  }
+	}, [selectedItem, isAddingItem])
+
+	if (isAddingItem) {
+		return <Form 
+			addItem={addItem} 
+			goHome={goHome} 
+		/>
+	}
+
+	if (selectedItem) {
+		return <Item {...selectedItem} 
+			goHome={goHome} 
+		/>
+	}
 
 	return (
 		<main>	
--- public/react/components/Form.jsx
+++ public/react/components/Form.jsx
@@ -1,32 +1,81 @@
 import React, { useEffect, useState } from 'react'
 import apiURL from '../api'
 
-export const Forms = ({ }) => {
+export const Forms = (props) => {
+  const [data, setData] = useState({
+      name: '',
+      price: '',
+      description: '',
+      category: '',
+      image: '',
+  })
+
+  function handleChange(event) {
+      setData({
+          ...data,
+          [event.target.name]: event.target.value
+      })
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      props.addItem(data)
+  };
+
   return <>
     <div className="overlay">
-      <form className='addForm'>
+      <button 
+        type="button"
+        className="backButton" 
+        onClick={props.goHome}
+      >
+        Back
+      </button>
+      <form className='addForm' onSubmit={handleSubmit}>
         <h2>Add Page</h2>
-        <input className='form-field'
+        <input 
+          className='form-field'
           placeholder='Name'
           type='text'
+          name='name'
+          id='name'
+          value={data.name}
+          onChange={handleChange}
         ></input>
-        <textarea className='form-field'
+        <textarea 
+          className='form-field'
           placeholder='Description'
           type='text'
+          name='description'
+          id='description'
+          value={data.description}
+          onChange={handleChange}
         ></textarea>
         <input className='form-field'
           placeholder='Category'
           type='text'
+          name='category'
+          id='category'
+          value={data.category}
+          onChange={handleChange}
         ></input>
         <input className='form-field'
           placeholder='Price'
           type='text'
+          name='price'
+          id='price'
+          value={data.price}
+          onChange={handleChange}
         ></input>
         <input className='form-field'
           placeholder='Img URL'
           type='text'
+          name='image'
+          id='image'
+          value={data.image}
+          onChange={handleChange}
         ></input>
-        <button className='button-create' type='submit'>Create Page</button>
+        <button className='button-create' type='submit'>Create Item</button>
       </form>
     </div>
   </>
