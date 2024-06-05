85cd57ebb5d2a4bb5e0b17cc7ba41a990dd22dad
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-06-05 20:29:34 +0100

    delete on single item view

--- public/react/Root.jsx
+++ public/react/Root.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Item } from './pages/Item'
 import { ItemsList } from './pages/ItemsList'
 import { App } from './components/App'
+import apiURL from './api';
 
-export const Root = () => (
-  <Router>
-    <Routes>
-      <Route path='/' element={<App />}>
-        <Route index element={<ItemsList />}/>
-        <Route path='item' element={<Item />}/>
-      </Route>
-    </Routes>
-  </Router>
-)
+export const Root = () => {
+  const [items, setItems] = useState([])
+  const [selectedItem, setSelectedItem] = useState(null)
+  const [formIsActive, setFormIsActive] = useState(false)
+
+  async function fetchItems(){
+		try {
+			const response = await fetch(`${apiURL}/items`);
+			const itemsData = await response.json();
+			
+			setItems(itemsData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+  
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<ItemsList 
+            items={items} 
+            fetchItems={fetchItems} 
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+            formIsActive={formIsActive}
+            setFormIsActive={setFormIsActive}
+          />}/>
+          <Route path='item' element={<Item 
+            fetchItems={fetchItems} 
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+            formIsActive={formIsActive}
+            setFormIsActive={setFormIsActive}
+          />}/>
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
--- public/react/pages/Item.jsx
+++ public/react/pages/Item.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation, Link } from 'react-router-dom'
-// import { useHistory } from 'react-router-dom'
+import { useLocation, Link, useNavigate } from 'react-router-dom'
+
 
 import apiURL from '../api'
 
-export const Item = () => {
+export const Item = ({ fetchItems }) => {
   const location = useLocation()
+  const navigate = useNavigate()
   const { item } = location.state
-  // const history = useHistory()
 
-  /*
   async function deleteItem (itemId) {
     try {
       const res = await fetch(`${apiURL}/items/${itemId}`, {
@@ -17,12 +16,12 @@ export const Item = () => {
       })
       await res.json()
       fetchItems()
-      // history.push('/')
+      navigate('/')
+
     } catch (err) {
       console.log('Oh no an error! ', err)
     }
   }
-  */
 
   return (
     <>
@@ -39,6 +38,7 @@ export const Item = () => {
             <div className='item-button-del-edt'>
               <button
                 className='item-button item-button-delete'
+                onClick={() => deleteItem(item.id)}
               >
                 Delete
               </button>
@@ -50,7 +50,7 @@ export const Item = () => {
             </div>
 
             <div className='item-buy'>
-              <h4 className='item-price'>{item.price}$</h4>
+              <h4 className='item-price'>£{item.price}</h4>
               <button className='item-button item-button-addtocart'>
                 Add to Cart
               </button>
